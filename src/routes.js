87c4c4a730c2655be4fb4e0430360ced0e3fe358
souz-dev <souz.developer@gmail.com>
2021-04-10 23:06:27 -0300

    Feature: Salvar Job/ Reamaning calc

--- src/routes.js
+++ src/routes.js
@@ -9,20 +9,78 @@ const profile = {
 	"monthly-budget": 3000,
 	"days-per-week": 5,
 	"hours-per-day": 5,
-	"vacation-per-year": 4
+	"vacation-per-year": 4,
+  "value-hours": 75
 }
 
-const jobs = []
+const jobs = [
+  {
+    id: 1,
+		name: 'Pizzaria Master pão',
+		'daily-hours': 2,
+		'total-hours': 1,
+    created_at: Date.now(),
+    
+  },
+  {
+    id: 2,
+		name: ' pão master pizza',
+		'daily-hours': 3,
+		'total-hours': 47,
+    created_at: Date.now()
+  }
+]
 
-routes.get('/', (req, res) =>  res.render(views +  'index', { profile }))
+function remainingDays(job){
+  const  remainingDays = (job['total-hours'] / job['daily-hours']).toFixed()
+
+  const createdDate = new Date(job.created_at)
+  const dueDay = createdDate.getDate() + Number(remainingDays)
+  const dueDate = createdDate.setDate(dueDay)
+
+  const timeDiffInMs = dueDate - Date.now()
+
+  // transform Ms in Day
+
+  const dayInMs = 1000 * 60 * 60 * 24
+  const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+
+  return dayDiff
+
+  //restam x dias
+}
+
+routes.get('/', (req, res) => { 
+  
+  const updateJobs = jobs.map((job) => {
+    
+    // calc dos Jobs
+    const remaining = remainingDays(job)
+    const status = remaining <= 0 ? 'done' : 'progress'
+
+    return {
+      ...job,
+      remaining,
+      status,
+      budget: profile["value-hours"] * job["total-hours"]
+    }
+  })
+  
+  return res.render(views +  'index', { profile, jobs:updateJobs  })
+})
 routes.get('/job', (req, res) =>  res.render(views +  'job'))
 routes.post('/job', (req, res) => {
 //  name: 'shuaashu', 'daily-hours': '5', 'total-hours': '5' }
 
-const job = req.body
+const lastId = jobs[jobs.length - 1]?.id || 1;
 
-
-	jobs.push()
+	jobs.push({
+    id: lastId + 1,
+		name: req.body.name,
+		'daily-hours': req.body['daily-hours'],
+		'total-hours': req.body['total-hours'],
+    created_at: Date.now()
+	})
 	return res.redirect('/') 
 })
 routes.get('/job/edit', (req, res) =>  res.render(views +  'job-edit'))
